98b13245f156ddd6faa31f65fb9dcf24537770aa
kh1279 <kh1279@admins-MacBook-Pro-2.local>
2015-06-05 12:15:30 +0530

    Initial Commit

---
+++ sampleDelegate/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sampleDelegate
+//
+//  Created by Chandrasekhar Pasumarthi on 05/06/15.
+//  Copyright (c) 2015 chandu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
